83172bbf033cb568918123df2ff21cba0f7547b0
lcarranco <larry@carrancomarketing.com>
2016-10-24 15:19:26 -0500

    Edited the white space

--- systemCalls.s
+++ systemCalls.s
@@ -1,19 +1,18 @@
 # A program that shows an example of using system calls
 
-    .data
+        .data
 str:
-    .asciiz "the answer = "
-    .text
-    .globl main
+        .asciiz "the answer = "
+        .text
+        .globl main
 
+main:   li		$v0, 4		    # System call code for print_str        # $v0 = 4
+        la		$a0, str		# Address of string to pring
+        syscall
 
-main:    li		$v0, 4		    # System call code for print_str        # $v0 = 4
-    la		$a0, str		# Address of string to pring
-    syscall
-
-    li		$v0, 1		    # System call code for print_int        # $v0 = 1
-    li		$a0, 5		    # Integer to print                      # $a0 = 5
-    syscall 
+        li		$v0, 1		    # System call code for print_int        # $v0 = 1
+        li		$a0, 5		    # Integer to print                      # $a0 = 5
+        syscall 
      
     
      
