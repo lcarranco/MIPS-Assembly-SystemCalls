ba010cab03771663f66e924a93492bf7674ba879
carranl <larry@carrancomarketing.com>
2016-10-24 15:01:26 -0500

    Initial source code

---
+++ systemCalls.s
@@ -0,0 +1,17 @@
+# A program that shows an example of using system calls
+
+    .data
+str:
+    .asciiz "the answer = "
+    .text
+    li		$v0, 4		    # System call code for print_str        # $v0 = 4
+    la		$a0, str		# Address of string to pring
+    syscall
+
+    li		$v0, 1		    # System call code for print_int        # $v0 = 1
+    li		$a0, 5		    # Integer to print                      # $a0 = 5
+    syscall 
+     
+    
+     
+    
